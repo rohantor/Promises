a27082f57e0a9fc1e2345af08df6405acca1f056
rohantor <rohan.magar@torinit.ca>
2023-01-10 13:14:23 +0530

    Promise all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
 import { useState,useEffect } from 'react';
-import {GetPromise} from './Demo2'
+import {GetPromise,GetPromiseAll} from './Demo2'
 
 import React from 'react';
 import Card from './Components/Card'
 
 function App() {
 const [value ,setValue ] = useState('Loading')
+const [all,setAll]  =useState([])
 
+const GetAllPromiseData = async()=>{
+
+   GetPromiseAll([1,2,3,4]).then((Data)=>{
+
+     setAll(Data)
+   })
+}
 useEffect(()=>{
+     setAll(new Array(4).fill('Loading'))
+      GetAllPromiseData()
+
+
+ 
  GetPromise().then((value )=>{
 setValue(value)
   })
@@ -20,6 +33,12 @@ setValue(value)
     <div className='App'>
       <div className='grid-container'>
         {typeof value === 'object' ? <Card value={value}/> : <h1>{value} </h1>}
+
+        {all.map((element)=>{
+            return (typeof element =='string'? <h4>Loading</h4>:<Card value={element}> </Card>  )
+
+           
+        })}
       </div>
     </div>
   )
--- src/Demo2.js
+++ src/Demo2.js
@@ -12,3 +12,39 @@ export const GetPromise =()=>{
       })
   })
 }
+
+export const GetPromiseById = (id) => {
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) => {
+        let jsonContain = res.json()
+        console.log(jsonContain)
+        return jsonContain
+      })
+      .then((res) => resolve(res))
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+export const GetPromiseArray = (arr) => {
+  let PromiseArray = []
+  for(let i = 0 ; i<arr.length;i++){
+PromiseArray.push(GetPromiseById(arr[i]))
+  }
+return PromiseArray;
+}
+
+export const GetPromiseAll = async(arr) => {
+  return Promise.all([...GetPromiseArray(arr)])
+    .then((res) => {
+      console.log({ res });
+      return res;
+    })
+    .catch((err) => {
+      console.log({ err });
+    });
+}
+
+
+
